e0a4dca132dc69e607c19ac2bf6080279ac6c8da
Yanai Avnet <bzakabz@gmail.com>
2019-02-14 16:10:10 +0200

    before first pull

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -2,6 +2,53 @@
 
 function init() {
     createMemes();
+    renderGallery();
 }
 
-function renderGallery()
+function renderGallery() {
+    var strHtml = '';
+    var memes = getMemesForDisplay();
+    if (!memes.length) strHtml += '<h2>no memes for display<h2>'
+    memes.forEach(meme => {
+        strHtml += `
+        <div class="gallery-item" style="background-image: url(${meme.src});" data-id="${meme.id}" onclick="onGalleryItemClicked(this)"></div>
+        `
+    });
+    document.querySelector('.img-gallery').innerHTML = strHtml;
+}
+
+function onGalleryItemClicked(elItem) {
+    var meme = getMemeById(elItem.dataset.id);
+    openEditor(meme);
+}
+
+function onSearchInput(val) {
+    filterMemes(val);
+    renderGallery();
+}
+
+function onAddMeme() {
+    document.querySelector('.modal.add-meme').classList.remove('hide');
+}
+
+function onSubmitNewMeme(ev, elForm) {
+    ev.preventDefault();
+    var inputName, inputTags, inputSrc;
+    [inputName, inputTags, inputSrc] = [].map.call(elForm.querySelectorAll('input'), function (input) { return input.value });
+    inputTags = inputTags.split(',').map(tag => tag.trim());
+    createMeme(inputName, inputSrc, ...inputTags);
+    document.querySelector('.modal.add-meme').classList.add('hide');
+    renderGallery();
+}
+
+function openEditor(meme) {
+    console.log(meme);
+}
+
+function onCloseModal() {
+    document.querySelector('.modal').classList.add('hide');
+}
+
+function onDisplaySearch() {
+    document.querySelector('.search-bar').classList.remove('hidden');
+}
--- js/gallery-service.js
+++ js/gallery-service.js
@@ -2,12 +2,14 @@
 
 var gMemes = loadFromStorage('gMemes') || [];
 
-function createMeme(name, id, ...tags) {
+function createMeme(name, src, ...tags) {
+    var regex = /[^\/]+$/;
     var meme = {
         name: name,
-        id: id,
-        src: id + '.jpg',
-        tags: tags
+        id: regex.exec(src)[0],
+        src: src,
+        tags: tags,
+        display: true
     }
     gMemes.push(meme);
 }
@@ -18,7 +20,24 @@ function saveMemes() {
 
 function createMemes() {
     if (gMemes.length) return;
-    createMeme('Dr. Evil Quotes', 'dr_evil', 'funny')
-    createMeme('Look At All These', 'look_at_all_these', 'funny', 'meme')
-    createMeme('Ancient Aliens', 'ancient_aliens', 'funny')
+    createMeme('Dr. Evil Quotes', 'img/meme-imgs/dr_evil.jpg', 'funny')
+    createMeme('Look At All These', 'img/meme-imgs/look_at_all_these.jpg', 'funny', 'meme')
+    createMeme('Ancient Aliens', 'img/meme-imgs/ancient_aliens.jpg', 'funny')
+}
+
+function getMemesForDisplay() {
+    return gMemes.filter(meme => meme.display);
+}
+
+function getMemeById(id) {
+    return gMemes.find(meme => meme.id === id);
+}
+
+function filterMemes(val) {
+    var regex = new RegExp(val, 'i');
+    if (!val) gMemes.forEach(meme => meme.display = true);
+    else gMemes.forEach(meme => {
+        if (regex.test(meme.name) || meme.tags.some(tag => regex.test(tag))) meme.display = true;
+        else meme.display = false;
+    });
 }
